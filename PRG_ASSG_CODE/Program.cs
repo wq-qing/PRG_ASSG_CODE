345ae564cf345565476b22a19e8c14f610ef4ade
Wan Cheng Tan <s10266775@connect.np.edu.sg>
2025-01-24 21:44:35 +0800

    airline partially completed

--- PRG_ASSG_CODE/Program.cs
+++ PRG_ASSG_CODE/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using PRG_ASSG_CODE;
+
+using (StreamReader airlinesFile = new StreamReader ("airlines.csv"))
+{
+    string? airline = airlinesFile.ReadLine ();
+    while ((airline =  airlinesFile.ReadLine()) != null)
+    {
+        string[] airlinesDetails = airline.Split (',');
+        string airlineName = airlinesDetails[0];
+        string airlineCode = airlinesDetails[1];
+        //continue only when flightList is created
+    }
+}
