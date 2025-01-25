d85a73c0e7c5ab39d4ccaaecc66752f766fb7749
zoeseah <zoesqz@yahoo.com>
2025-01-25 11:47:26 +0800

    Created DDJBFlights Class

---
+++ PRG_ASSG_CODE/DDJBFlight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRG_ASSG_CODE;
+
+namespace PRG_ASSG_CODE
+{
+    class DDJBFlight:Flight
+    {
+        public double RequestFee { get; set; }
+        public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time", double requestFee = 300):base(flightNumber, origin, destination, expectedTime, status)
+        {
+             RequestFee=requestFee;
+        }
+        public override double CalculateFees()
+        {
+            return base.CalculateFees()+RequestFee;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Type: DDJB Flight, Fee: {CalculateFees()}"; 
+        }
+    }
+}
