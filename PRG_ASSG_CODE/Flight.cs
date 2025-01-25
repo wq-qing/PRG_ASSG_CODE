ab2640d593f78d92ddeefea90e61dab0b632b378
zoeseah <zoesqz@yahoo.com>
2025-01-25 12:29:28 +0800

    Added Flight Class

--- PRG_ASSG_CODE/Flight.cs
+++ PRG_ASSG_CODE/Flight.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace PRG_ASSG_CODE
 {
-    internal class Flight
+    class Flight
     {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
 
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time")
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
+
+        public virtual double CalculateFees()
+        {
+            return 500; 
+        }
+
+        public override string ToString()
+        {
+            return $"Flight: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, Expected Time: {ExpectedTime}, Status: {Status}";
+        }
     }
+
 }
