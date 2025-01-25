cb7e458c9e10947b7a311a7750f88c0307223aef
zoeseah <zoesqz@yahoo.com>
2025-01-25 13:03:12 +0800

    Added LWTTFlight Class

---
+++ PRG_ASSG_CODE/LWTTFlight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_ASSG_CODE
+{
+    class LWTTFlight:Flight
+    {
+        public double requestFee {  get; set; }
+        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time", double requestFee = 500)
+        : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = requestFee;
+        }
+    }
+}
